4f27583b299910fa2c2b937dfc072304ba3de800
h3lgi <o.kamenscic@gmail.com>
2020-10-24 22:40:40 +0300

    init structure

---
+++ colorSortingLine.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#include "config.h"
+
+void setup()
+{
+  
+}
+
+
+void loop()
+{
+  feedMechLoop();
+  lineControlLoop();
+  colorRecognitionLoop();
+  armControlLoop();
+  
+}
---
+++ config.h
@@ -0,0 +1,11 @@
+//connection configs 
+
+//output congfigs
+
+//robot arm control config
+
+//color recognition config
+
+//line control config
+
+//feedMech config
