f173a410d7f1131fe1177c719bfdbc5dfaab96f2
h3lgi <o.kamenscic@gmail.com>
2020-10-25 18:37:58 +0200

    add some configs

--- config.h
+++ config.h
@@ -1,11 +1,23 @@
 //connection configs 
+bool objectsAvailible;
+
 
 //output congfigs
 
 //robot arm control config
 
 //color recognition config
+typedef struct 
+{
+  uint8_t R;
+  uint8_t G;
+  uint8_t B;
+}color;
 
 //line control config
+
+bool emergencyStop;
+bool wrongObject; 
+bool slower;
 
 //feedMech config
