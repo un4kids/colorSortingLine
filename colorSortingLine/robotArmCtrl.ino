0f10ab2166b05dadc58b838c381283f002ad5fe9
TOT102 <73033834+TOT102@users.noreply.github.com>
2020-11-08 19:17:14 +0300

    Update robotArmCtrl.ino

--- colorSortingLine/robotArmCtrl.ino
+++ colorSortingLine/robotArmCtrl.ino
@@ -1,6 +1,30 @@
+#define sensPin 12
+int ObjectColor;
+bool availableObject = false;
 
+void isObjectToSortIsPresent(){
+  if (digitalRead(sensPin) == HIGH){
+    availableObject = true;
+  }
+}
+
+void sortCurrentObject(){
+  switch (ObjectColor){
+    case 1:
+    availableObject = false;
+    break;
+    case 2:
+    availableObject = false;
+    break;
+    case 3:
+    availableObject = false;
+    break;
+  }
+}
 
 void armControlLoop()
 {
-  
+  if (availableObject = true){
+   sortCurrentObject(); 
+  }
 }
