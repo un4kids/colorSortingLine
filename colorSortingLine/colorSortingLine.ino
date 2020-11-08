1cd18683228027560116961907beb44ed46a2b59
PavelPLP <73434898+PavelPLP@users.noreply.github.com>
2020-11-08 18:31:17 +0200

    EmergencyStop
    EmergencyStop

--- colorSortingLine/colorSortingLine.ino
+++ colorSortingLine/colorSortingLine.ino
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 
 #include "config.h"
-
+int lm1 = 4;
+int lm2 = 5;
+pinMode(lm_1,OUTPUT) ;   
+pinMode(lm_2,OUTPUT) ;
 void setup()
 {
   
--- colorSortingLine/lineControl.ino
+++ colorSortingLine/lineControl.ino
@@ -2,5 +2,14 @@
 
 void lineControlLoop()
 {
-  
+
+}
+
+void emergencyStop()
+{
+  while (emergencyStop = true)
+  {
+    digitalWrite(in_1, LOW) ;
+    digitalWrite(in_2, LOW) ;
+  }
 }
