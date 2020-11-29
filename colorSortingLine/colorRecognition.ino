10c194fd5438b4a6581726b8aca5ccf5c8c9a85f
petya2003 <73399631+petya2003@users.noreply.github.com>
2020-11-29 18:40:59 +0200

    73399631+petya2003@users.noreply.github.com

--- colorSortingLine/colorRecognition.ino
+++ colorSortingLine/colorRecognition.ino
@@ -1,6 +1,25 @@
 
-
-void colorRecognitionLoop()
+void colorRecognitionbegin()
 {
   
+}
+void colorRecognitionLoop()
+{
+  if(isObjectPresent())
+  {
+    Serial.println("is present");
+  }
+  else{
+     Serial.println("is not present");
+  }
+  
+
+
+}
+bool isObjectPresent() {
+  if (digitalRead(2) == LOW)
+  {
+    return true;
+  }
+  return false;
 }
--- colorSortingLine/config.h
+++ colorSortingLine/config.h
@@ -7,12 +7,21 @@ bool objectsAvailible;
 //robot arm control config
 
 //color recognition config
-typedef struct 
+typedef struct color_t
 {
   uint8_t R;
   uint8_t G;
   uint8_t B;
 }color;
+struct cube
+{
+  color_t;
+}
+#define A0 A0
+void colorRecognation1() {
+Serial.begin (115200);
+
+}
 
 //line control config
 
