c003e633e7d3467e9524876504f0eb985906c217
Oleg <o.kamenscic@gmail.com>
2020-11-29 18:28:48 +0200

    review

--- colorSortingLine/robotArmCtrl.ino
+++ colorSortingLine/robotArmCtrl.ino
@@ -1,32 +1,36 @@
 #define sensPin 12
 int ObjectColor;
+
+
+
+
 bool availableObject = false;
 
-void isObjectToSortIsPresent(){
-  if (digitalRead(sensPin) == HIGH){
+void isObjectToSortIsPresent() {
+  if (digitalRead(sensPin) == HIGH) {
     availableObject = true;
   }
 }
 
-void sortCurrentObject(){
-  switch (ObjectColor){
+void sortCurrentObject() {
+  switch (ObjectColor) {
     case 1:
-    availableObject = false;
-    break;
+      availableObject = false;
+      break;
     case 2:
-    availableObject = false;
-    break;
+      availableObject = false;
+      break;
     case 3:
-    availableObject = false;
-    break;
+      availableObject = false;
+      break;
     case 0:
-    availableObject = false;
-    break;
+      availableObject = false;
+      break;
   }
 }
 
-void armControlLoop(){
-  if (availableObject = true){
-   sortCurrentObject(); 
+void armControlLoop() {
+  if (availableObject) {
+    sortCurrentObject();
   }
 }
