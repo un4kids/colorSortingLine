f211201e12ebf6e47dad4b49d3e71ffc96929876
petya2003 <73399631+petya2003@users.noreply.github.com>
2020-11-29 18:49:59 +0200

    73399631+petya2003@users.noreply.github.com

--- colorSortingLine/colorRecognition.ino
+++ colorSortingLine/colorRecognition.ino
@@ -1,10 +1,22 @@
 
 void colorRecognitionbegin()
 {
-  
+  for (byte i=0;i<100;i++)
+  {
+     Serial.println cube[i].color.r = random (0,255);
+   Serial.println cube[i].color.g = random (0,255);
+   Serial.println cube[i].color.b = random (0,255);
+  }
+  Serial.begin(115200)
 }
+
 void colorRecognitionLoop()
+
 {
+  
+  Serial.println (cube[i].color.r );
+   Serial.println (cube[i].color.g);
+   Serial.println (cube[i].color.b);
   if(isObjectPresent())
   {
     Serial.println("is present");
