1bfe4f5d232990ef9f35ac408cda4b53a876c5dc
TOT102 <73033834+TOT102@users.noreply.github.com>
2021-02-07 19:23:35 +0200

    Add eeprom

--- colorSortingLine/colorSortingLine.ino
+++ colorSortingLine/colorSortingLine.ino
@@ -1,10 +1,40 @@
-#include <Arduino.h>
-
+#include <EEPROMex.h>
+#include <Arduino.h>                                                                                            
+#include "A4988.h"
+#include "MultiDriver.h"
+#include "SyncDriver.h"
 #include "config.h"
+//ArmControl
+int ObjectColor1;
+int ObjectColor2;
+int ObjectColor3;
+bool needToSafe = true;
+
+struct ArmSettings
+ { 
+  int Movement1;
+  int Movement2;
+  int Movement3;
+ };
+
+ArmSettings settings = {22, 6, 576};
+
+char ObjectColor;
+bool availableObject = false;
+
+A4988 xMotor(STEPS_PER_REVOLUTION, X_DIR, X_STP, EN);
+A4988 yMotor(STEPS_PER_REVOLUTION, Y_DIR, Y_STP, EN);
+A4988 zMotor(STEPS_PER_REVOLUTION, Z_DIR, Z_STP, EN);
+uint8_t movementMode = 0;
+unsigned xWaitInMicros = 0;
+unsigned yWaitInMicros = 0;
+unsigned zWaitInMicros = 0;
+SyncDriver ctl(xMotor, yMotor, zMotor);
+
 
 void setup()
 {
-  
+  setupArm();
 }
 
 
--- colorSortingLine/robotArmCtrl.ino
+++ colorSortingLine/robotArmCtrl.ino
@@ -1,11 +1,3 @@
-#define sensPin 12
-int ObjectColor;
-
-
-
-
-bool availableObject = false;
-
 void isObjectToSortIsPresent() {
   if (digitalRead(sensPin) == HIGH) {
     availableObject = true;
@@ -13,24 +5,72 @@ void isObjectToSortIsPresent() {
 }
 
 void sortCurrentObject() {
+  Serial.print(ObjectColor);
   switch (ObjectColor) {
-    case 1:
-      availableObject = false;
+    case MOVE_1:
+      xMotor.enable();
+      ctl.rotate(0, 18, 12);
+      delay(200);
       break;
-    case 2:
-      availableObject = false;
+    case MOVE_2:
+      ctl.rotate(0, -4, -8);
+      ctl.rotate(2, 4, 8);
+      delay(200);
       break;
-    case 3:
-      availableObject = false;
+    case MOVE_3:
+      // ctl.rotate(2, 4, 8);
+      ctl.rotate(0, -4, -8);
+      ctl.rotate(2, 4, 8);
+      delay(200);
       break;
-    case 0:
-      availableObject = false;
-      break;
+    case HOME:
+      ctl.rotate(-6.0, -14.5, -4.5);
+      delay(200);
+      xMotor.disable();
   }
 }
 
 void armControlLoop() {
   if (availableObject) {
     sortCurrentObject();
+    availableObject = false;
   }
+}
+
+void setupArm() {
+  pinMode(X_DIR, OUTPUT); pinMode(X_STP, OUTPUT);
+  pinMode(Y_DIR, OUTPUT); pinMode(Y_STP, OUTPUT);
+  pinMode(Z_DIR, OUTPUT); pinMode(Z_STP, OUTPUT);
+
+  xMotor.begin(X_RPM, MICROSTEPS);
+  yMotor.begin(Y_RPM, MICROSTEPS);
+  zMotor.begin(Z_RPM, MICROSTEPS);
+  xMotor.setEnableActiveState(LOW);
+  yMotor.setEnableActiveState(LOW);
+  zMotor.setEnableActiveState(LOW);
+
+  xMotor.setSpeedProfile(xMotor.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
+  yMotor.setSpeedProfile(yMotor.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
+  zMotor.setSpeedProfile(zMotor.LINEAR_SPEED, 600, 600);
+
+  pinMode(EN, OUTPUT);
+  movementMode = 1;
+  Serial.begin(115200);
+}
+void saveArmSettings(ArmSettings* as, int address)
+{
+  EEPROM.writeInt(address, as->Movement1);
+  address += sizeof(int);
+  EEPROM.writeInt(address, as->Movement2);
+  address += sizeof(int);
+  EEPROM.writeInt(address, as->Movement3);
+}
+
+void readArnSettings(ArmSettings* as, int address)
+{
+ Serial.println(EEPROM.readInt(address));
+ address += sizeof(int);
+ Serial.println(EEPROM.readInt(address));
+ address += sizeof(int);
+ Serial.println(EEPROM.readInt(address));
 }
