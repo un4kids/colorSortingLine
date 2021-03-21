0199b66537126bb4e4f8d9f003b1fdab95926401
Rangel <rangelplachkov1@gmail.com>
2021-03-21 18:30:38 +0200

    Update colorSortingLine.ino

--- colorSortingLine/colorSortingLine.ino
+++ colorSortingLine/colorSortingLine.ino
@@ -1,14 +1,8 @@
 #include <Arduino.h>
 #include "config.h"
-#include <EEPROMex.h>
 #define sensPin 12 
 int ObjectColor;
 int EEPROMaddr = 0;
-
-
-//feedMechParams
-//Object_color
-//lineControlparams
 void setup()
 {
   
