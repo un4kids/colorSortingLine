4316d412b119cfa4547180ea6cd26056f38949a5
Strahil <straho949@gmail.com>
2021-04-25 17:54:18 +0300

    Create remoteCtl.ino

--- colorSortingLine/robotArmCtrl.ino
+++ colorSortingLine/robotArmCtrl.ino
@@ -1,35 +1,35 @@
-bool isObjectToSortIsPresent(){
-  if (digitalRead(sensPin) == HIGH){
-    return true;
-  }
-  return false;
-  
-}
+//bool isObjectToSortIsPresent(){
+//  if (digitalRead(sensPin) == HIGH){
+//    return true;
+//  }
+//  return false;
+//  
+//}
 
-void sortCurrentObject(){
-  switch (ObjectColor){
-    case 1:
-    //move to position
-    isObjectToSortIsPresent() = false;
-    break;
-    case 2:
-    //move to position
-    isObjectToSortIsPresent() = false;
-    break;
-    case 3:
-    //move to position
-    isObjectToSortIsPresent() = false;
-    break;
-    case 0:
-    //move to position
-    isObjectToSortIsPresent() = false;
-    break;
-  }
-}
+//void sortCurrentObject(){
+//  switch (ObjectColor){
+//    case 1:
+//    //move to position
+//   // isObjectToSortIsPresent() = false;
+//    break;
+//    case 2:
+//    //move to position
+//   // isObjectToSortIsPresent() = false;
+//    break;
+//    case 3:
+//    //move to position
+//    //isObjectToSortIsPresent() = false;
+//    break;
+//    case 0:
+//    //move to position
+//   // isObjectToSortIsPresent() = false;
+//    break;
+//  }
+//}
 
-void armControlLoop(){
-  if (isObjectToSortIsPresent()){
-   sortCurrentObject(); 
-   //sort
-  }
-}
+//void armControlLoop(){
+//  if (isObjectToSortIsPresent()){
+//   sortCurrentObject(); 
+//   //sort
+//  }
+//}
